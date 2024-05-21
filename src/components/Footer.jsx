4e00e12cb1bd2e2b66a662fef53ccda59ecd5df7
xezernn <xezernovruz79@gmail.com>
2024-05-21 23:29:04 +0400

    Header and footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,62 @@
+"use client"
+
+import Link from 'next/link'
+import React from 'react'
+import { FaFacebookF, FaInstagram, FaTripadvisor, FaTwittet } from 'react-icons/fa'
+
+function Footer() {
+    return (
+        <footer>
+            <div className="container">
+                <div id='copyElement'>
+                    <div className='copy'>
+                        © PJA 2023
+                    </div>
+                </div>
+                <div className='footerLeft'>
+                    <ul id='footerMenu'>
+                        <li><Link href="">Haqqımızda</Link></li>
+                        <li><Link href="">Sual-Cavab</Link></li>
+                        <li><Link href="">Karyera</Link></li>
+                        <li><Link href="">Xəmir</Link></li>
+                        <li><Link href="">Papa Talk - Şərtlər və Qaydalar</Link></li>
+                    </ul>
+                    <ul id='footerMenu' className='footerright'>
+                        <li><Link href="">Restoranlar</Link></li>
+                        <li><Link href="">Kampaniyalar</Link></li>
+                        <li><Link href="">PapaBonus</Link></li>
+                    </ul>
+                </div>
+                <div className="footerBottom">
+                    <div className="footerIcons">
+                        <a href="tel:+99470923313"><img src="https://www.papajohns.az/img/content/footer-images-az-AZ.png" alt="tel" /></a>
+                    </div>
+                    <div className="socialIcons">
+                        <ul id='socialMenu'>
+                            {/* <li>
+                                <a href="https://www.facebook.com/papajohns.az/"><FaFacebookF /></a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/azpapajohns"><FaTwittet /></a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/azpapajohns/"><FaInstagram /></a>
+                            </li>
+                            <li>
+                                <Link href=""><FaTripadvisor /></Link>
+                            </li> */}
+                        </ul>
+                    </div>
+                    <div id='copyElement'>
+                        <div className='copy'>
+                            © PJA 2023
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import Image from 'next/image'
+import { FaQuestion, FaUser } from "react-icons/fa";
+import { RiShoppingBasket2Fill } from "react-icons/ri";
+import Link from 'next/link';
+import logo from '@/assets/img/logo.png';
+
+function Header() {
+    const say = ""
+
+    return (
+        <header>
+            <div className='container'>
+                <div className="headerTop">
+                    <Link href="/" id='logo'>
+                        {/* <Image src={logo} alt="logo" /> */}
+                        <img src='https://www.papajohns.az/img/content/pj_logo_web_new.png' alt="logo" />
+                    </Link>
+                    <div className='topMenu'>
+                        <ul>
+                            <li><Link href="#">Restoranlar</Link></li>
+                            <li><Link href="#">PapaBonus</Link></li>
+                        </ul>
+                    </div>
+                    <div className="headerLogin">
+                        <div className='headerIcon'>
+                            <img id='azFlag' src="https://www.papajohns.az/img/content/flag_az.png" alt="AZ Flag" />
+                            <p className='textMobil'>Az</p>
+                            <p className='textDesk'>Azərbaycanca</p>
+                        </div>
+                        <div className='headerIcon'>
+                            <FaUser className='icon' />
+                            <p className='textMobil'>Giriş</p>
+                            <p className='textDesk'>Giriş / Qeydiyyat</p>
+                        </div>
+                        <div className='headerIcon flexCol'>
+                            <RiShoppingBasket2Fill className='icon' />
+                            <p>{say.toString().length > 4 ? say.toFixed(2) : say}₼</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* header bottom burdan baslayir */}
+
+            <div className='headerRed'>
+                <div className="container">
+                    <div id='redBar'>
+                        <Link href="/" id='redLogo'>
+                            <Image src={logo} alt="logo" />
+                        </Link>
+                        <ul id='menuList'>
+                            <li><Link href="#">Kampaniyalar</Link></li>
+                            <li><Link href="#">Papadias</Link></li>
+                            <li className="active"><Link href="#">Pizza</Link></li>
+                            <li><Link href="#">Qəlyanaltı</Link></li>
+                            <li><Link href="#">Salat</Link></li>
+                            <li><Link href="#">Pasta</Link></li>
+                            <li><Link href="#">İçki</Link></li>
+                            <li><Link href="#">Desert</Link></li>
+                            <li><Link href="#">Sous</Link></li>
+                        </ul>
+                        <span id='bal'>
+                            0 Bal
+                            <FaQuestion />
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
